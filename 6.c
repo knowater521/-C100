53e3cd0ce1297b632cdabc8886a38185e37d8443
iamyourhang <44526115+iamyourhang@users.noreply.github.com>
2019-09-20 19:45:18 +0800

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,17 @@
+/*
+	用*画画 用*号输出字母C的图案。
+*/
+#include <stdio.h>
+
+int main()
+{
+	printf(" ***  *****\n");
+	printf("*       *\n");
+	printf("*       *\n");
+	printf(" ***    *\n");
+
+
+	return 0;
+
+
+}
